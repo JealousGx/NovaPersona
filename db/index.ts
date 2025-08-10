080f163f5d825acc314ebc74ec8d7c3e7efb20d8
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2025-08-10 13:53:10 +0500

    refactor: simplify database connection and remove unused SQLite code

--- db/index.ts
+++ db/index.ts
@@ -1,20 +1,3 @@
-import { drizzle as drizzleSQLite } from "drizzle-orm/libsql";
 import { drizzle as drizzleMySQL } from "drizzle-orm/mysql2";
 
-let db;
-
-if (
-  process.env.DB_TYPE === "sqlite" ||
-  process.env.NODE_ENV === "development"
-) {
-  db = drizzleSQLite({
-    connection: {
-      url: process.env.DATABASE_URL!,
-      authToken: process.env.DATABASE_AUTH_TOKEN!,
-    },
-  });
-} else {
-  db = drizzleMySQL(process.env.DATABASE_URL!);
-}
-
-export { db };
+export const db = drizzleMySQL(process.env.DATABASE_URL!);
