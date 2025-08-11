e83ef2f6cf528f264cb37bf20a75280c368531b4
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2025-08-11 21:39:53 +0500

    fix: update drizzle initialization to include schema and mode parameters

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
+import * as schema from "@/db/schema";
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST!,
   port: Number(process.env.DB_PORT!),
@@ -9,6 +11,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME!,
 });
 
-export const db = drizzle({
-  client: pool,
+export const db = drizzle(pool, {
+  schema,
+  mode: "default",
 });
